04eb57d3b641f301877804b0e0e188ef52bc11ec
Vishudh Shah <vishudh@hotmail.com>
2025-08-30 16:19:18 +0530

    Added projects 1-4

--- assets/js/script.js
+++ assets/js/script.js
@@ -184,6 +184,8 @@ form.addEventListener("submit", function(e) {
 
 
 
+// PAGE NAVIGATION LOGIC
+
 // page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
